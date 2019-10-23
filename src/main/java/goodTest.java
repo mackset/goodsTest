6758125aa0494e46ba2c9de2edd1720515e029a6
LindaCy <2296782218@qq.com>
2019-10-23 18:00:24 +0800

    提交成功了

---
+++ src/main/java/goodTest.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * @ClassName goodTest
+ * @Description
+ * @Date 2019/10/23 17:55
+ * @Author zhangtao
+ */
+
+public class goodTest {
+    public static void main(String[] args) {
+        System.out.print("sss");
+    }
+}
